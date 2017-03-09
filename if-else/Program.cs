b99239a8b3fa0b7f4c83ea9140738c3fae080166
Alfredo Granado <alfred_grand@outlook.com>
2017-03-08 23:04:37 -0500

    this ois the last change to make the code shorter

--- if-else/Program.cs
+++ if-else/Program.cs
@@ -22,28 +22,19 @@ namespace if_else
         {
             
             if (userValue == "1")
-            {
-                Console.WriteLine("you won a car");
-            }
+             Console.WriteLine("you won a car");
+            
             else if (userValue == "2")
-            {
-                Console.WriteLine("you won Pheobe, the bad the pussy");
-            }
+             Console.WriteLine("you won Pheobe, the bad the pussy");
+            
             else if (userValue == "3")
-            {
-                Console.WriteLine("you won Aventer the lazy cat");
-
-            }
-
+             Console.WriteLine("you won Aventer the lazy cat");
+                       
             else if (userValue != "1" && userValue != "2" && userValue != "3") 
-            {
-                Console.WriteLine("I told you put a value from 1 to 3. Am I speaking spanish? ");
-            } 
+              Console.WriteLine("I told you put a value from 1 to 3. Am I speaking spanish? ");
+          
             else
-            {
-                Console.WriteLine("I speak in spanish whe I get mad. Te dije que pusieras un numero del 1 al 3 como puta madre tendre que decirlo");
-            }        
-
+              Console.WriteLine("I speak in spanish whe I get mad. Te dije que pusieras un numero del 1 al 3 como puta madre tendre que decirlo");
         }
 
     }  
